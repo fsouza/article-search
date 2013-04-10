3a30aa5c59990e0fe416e0544d9640c48991deb6
Francisco Souza <f@souza.cc>
2013-04-09 23:30:18 -0300

    main: add main program
    This program is used as a interface specification.

---
+++ main.go
@@ -0,0 +1,31 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"github.com/fsouza/article-search/search"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	var query string
+	store, err := search.NewStore(os.Args[1:]...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		fmt.Print("Search for: ")
+		fmt.Scanf("%s", &query)
+		articles, err := store.Search(query)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Printf("Matching articles: %s.\n", strings.Join(articles, ", "))
+	}
+}
